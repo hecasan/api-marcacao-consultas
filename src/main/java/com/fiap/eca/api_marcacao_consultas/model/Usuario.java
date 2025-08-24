10e58447dfa61198054806933cd2628215ee4a85
Hete Caetano <hetecaetano@gmail.com>
2025-08-24 20:56:09 -0300

    feat: atualizar o sinalizador de inicialização do banco de dados e melhorar a lógica de inicialização de dados

--- src/main/java/com/fiap/eca/api_marcacao_consultas/model/Usuario.java
+++ src/main/java/com/fiap/eca/api_marcacao_consultas/model/Usuario.java
@@ -1,6 +1,8 @@
 package com.fiap.eca.api_marcacao_consultas.model;
+
 import jakarta.persistence.*;
 import lombok.Data;
+
 @Data
 @Entity
 @Table(name = "usuarios")
@@ -12,4 +14,5 @@ public class Usuario {
     private String email;
     private String senha;
     private String tipo; // Paciente, Médico ou Admin
+    private String especialidade; // Especialidade do médico (null para pacientes e admin)
 }
